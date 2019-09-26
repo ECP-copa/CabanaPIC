374457e5411b817a5d27f28e8ffaa083d26d3c17
Guangye Chen <gchen@lanl.gov>
2019-09-26 16:43:24 -0600

    updated the 2stream-em test (included 2-thread result)

--- src/initializer.h
+++ src/initializer.h
@@ -49,7 +49,7 @@ class Initializer {
             real_t gam = 1.0/sqrt(1.0-Parameters::instance().v0*Parameters::instance().v0);
 	      
             Parameters::instance().len_x_global = default_grid_len;
-            Parameters::instance().len_y_global = 3.1415926; //6.28318530717959; //default_grid_len;
+            Parameters::instance().len_y_global = 3.14159265358979*0.5; //6.28318530717959; //default_grid_len;
             Parameters::instance().len_z_global = default_grid_len;
 
             Parameters::instance().len_x = Parameters::instance().len_x_global; //default_grid_len;
@@ -105,7 +105,10 @@ class Initializer {
                     size_t pic = (2*pi)%nppc;
 
                     real_t x = pic*dxp+0.5*dxp-1.0; //rand_float(-1.0f, 1.0f); //
-		    x += dxp*rand_float(0.0f, 0.002f);
+                    size_t pre_ghost = (2*pi/nppc);
+                    //real_t na = 1e-6*sin(6.28318530717959*((x+1.0+pre_ghost*2)/(2*ny)));
+		    //x += dxp*na;
+		    //x += dxp*rand_float(0.0f, 0.002f);
                     //real_t x = rand_float(-1.0f, 1.0f); //
                     position_x.access(s,i) = 0;
                     position_y.access(s,i) = x; //rand_float(-1.0f, 1.0f);
@@ -118,20 +121,20 @@ class Initializer {
                     //int pre_ghost = (s % Parameters::instance().num_real_cells);
                     //   size_t ix, iy, iz;
 
-                    size_t pre_ghost = (2*pi/nppc);
+
 
                     //cell.access(s,i) = pre_ghost + (nx+2)*(ny+2) + (nx+2) + 1; //13; //allow_for_ghosts(pre_ghost);
 		    //test y
                     cell.access(s,i) = pre_ghost*(nx+2) + (nx+2)*(ny+2) + (nx+2) + 1; 
                     // Initialize velocity.(each cell length is 2)
-                    real_t na = 1e-2*sin(6.28318530717959*((x+1.0+pre_ghost*2)/(2*nx)));
+
                     
 
                     real_t gam = 1.0/sqrt(1.0-v0*v0);
-                    velocity_x.access(s,i) = sign * v0*gam *(1.0-na*sign); //0;
+                    velocity_x.access(s,i) = sign * v0*gam; // *(1.0-na*sign); //0;
                     velocity_y.access(s,i) = 0;
-                    velocity_z.access(s,i) = 0;  //sign * v0 *gam*(1.0+na*sign);
-		    //if(pre_ghost*2==ny) velocity_x.access(s,i) = velocity_x.access(s,i)*(1.0-0.01*sign);
+                    velocity_z.access(s,i) = 0; //na*sign;  //sign * v0 *gam*(1.0+na*sign);
+		    velocity_z.access(s,i) = 1e-7*sign;
                 };
 
             Cabana::SimdPolicy<particle_list_t::vector_length,ExecutionSpace>
--- test/2-stream-em/plot.gp
+++ test/2-stream-em/plot.gp
@@ -6,7 +6,7 @@ set output '2stream-em.png'
 set xlabel '{/Symbol w}_pt'
 set ylabel 'W_B'
 set xrange [0:150]
-set yrange [1e-14:10]
+set yrange [1e-15:10]
 set log y
 set grid
 set format y "%.e"
@@ -14,5 +14,5 @@ f(x) = exp(0.279*2*x)
 set keytitle '({/Symbol g}_0=1.02), nx=32, nppc=100'
 set key bottom
 
-plot 'outw0.2' u 2:4 w l t 'minipic 1-thread',f(x)*5e-15 t 'linear theory' 
+plot 'outw0.2' u 2:4 w l t 'minipic 1-thread','outw0.2-2' u 2:4 w l t 'minipic 2-thread',f(x)*1e-16 t 'linear theory' 
 
